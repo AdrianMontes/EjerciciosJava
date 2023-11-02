651f5c946e54adb3fcff38307f19f4c3a65788f8
AdrianMontes <adrian.montes01@estudiant.upf.edu>
2023-11-02 12:21:51 +0100

    Primeros ejercicios de Java hechos

---
+++ Ejercicio_pag40/src/Ejercicio40/Ejercicio40.java
@@ -0,0 +1,9 @@
+package Ejercicio40;
+
+public class Ejercicio40 {
+
+	public static void main(String[] args) {
+		System.out.println("I<3Java");
+	}
+
+}
---
+++ Ejercicio_pag60/src/Ejercicio60/Ejercicio60.java
@@ -0,0 +1,21 @@
+package Ejercicio60;
+
+public class Ejercicio60 {
+
+	public static void main(String[] args) {
+		System.out.println("Salto\nde\nlinea");
+		System.out.println("----------------");
+		System.out.println("Ta\tbu\tla\tdor");
+		System.out.println("----------------");
+		System.out.println("\\Diagonal inversa\\");
+		System.out.println("----------------");
+		System.out.println("\"Comillas dobles\"");
+		System.out.println("----------------");
+		System.out.println("\'Comilla simple\'");
+		System.out.println("----------------");
+		System.out.println("Retorno\r de carro");
+		System.out.println("----------------");
+		System.out.println("Borrado \ba\b la izquierda");
+	}
+
+}
